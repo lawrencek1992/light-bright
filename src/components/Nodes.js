2b0717ad05a853607dfe42e8c3af6cc19eec7771
Kelly <lawrencek1992@gmail.com>
2021-06-24 20:48:59 -0600

    Update Node.js

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Button } from 'reactstrap'; 
-
+import { 
+    Container, 
+    Button 
+} from 'reactstrap'; 
 
 const Nodes = () => {
-    return (
-        <div>
+    const mapNodes = () => {
+        let numNodes = 100;
+        let i = 0;
+        while (i <= numNodes) {
+            let nodeID = "node" + i;
+            i++
+            return (
+                <Button id={nodeID} className="node" />
+            );
+        }
+    }
 
-        </div>
+    return (
+        <Container className="Nodes" fluid>
+            {mapNodes}
+            {/* <Button id="node1" className="node" /> */}
+        </Container>
     );
 };
 
