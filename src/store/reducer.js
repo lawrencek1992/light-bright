a2637b8bb57a20f154cf49037d745e8ae41ec158
Kelly <lawrencek1992@gmail.com>
2021-06-26 21:47:12 -0600

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,2 +1,26 @@
 import { STORE, UPDATE, RESET } from './actionTypes';
 import { initialData } from './initialData';
+
+export default function reducer (storeData, action) {
+    switch (action.type) {
+        case STORE:
+            return {
+                ...storeData,
+                currentColor: action.newColor
+            }
+        case UPDATE: 
+            return {
+                ...storeData,
+                action.node: action.newColor
+            }
+        case RESET:
+            let updatedStoreData = {}
+            for (item in storeData) {
+                item = "#343a40";
+            }
+            return updatedStoreData;
+        default: 
+            return storeData || initialData;
+        ;
+    }
+}
