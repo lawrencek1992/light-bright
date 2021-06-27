cd0763a232d9a343becb41b3e7644d32e08c1354
Kelly <lawrencek1992@gmail.com>
2021-06-26 20:42:41 -0600

    Create actionCreators.js

---
+++ src/store/actionCreators.js
@@ -0,0 +1,13 @@
+import { STORE, UPDATE, RESET } from './actionTypes';
+
+export const storeColor = (color) => {
+
+};
+
+export const upDateColor = (node) => {
+
+};
+
+export const resetColors = () => {
+
+};
