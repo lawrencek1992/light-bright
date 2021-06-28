da0c98291c82b6206063990959c4bc95cd475747
Kelly <lawrencek1992@gmail.com>
2021-06-27 18:49:58 -0600

    Add Provider to index.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './style.scss';
 import Header from './components/Header';
 import Nodes from './components/Nodes';
-import { Provider } from 'react-redux';
-import store from './store';
 
 const App = () => {
   return (
-    <Provider store={store}>
       <div className="App">
         <Header />
         <Nodes />
       </div>
-    </Provider>
   );
 }
 
