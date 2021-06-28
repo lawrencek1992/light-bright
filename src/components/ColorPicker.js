db58b9703a3f48a2b13343fea26828acc8727feb
Kelly <lawrencek1992@gmail.com>
2021-06-27 21:33:28 -0600

    Change icon in header to show currentColor

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import { STORE } from '../store/actionTypes';
+import {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PINK,
+    PURPLE,
+    BROWN,
+    BLACK,
+    WHITE
+} from '../store/colors';
+import { useDispatch } from 'react-redux';
 import { 
     Button, 
     ButtonGroup,
@@ -9,6 +23,8 @@ import {
 } from 'reactstrap';
 
 const ColorPicker = () => {
+    const dispatch = useDispatch();
+
     const largeScreens = window.matchMedia("(max-width: 992px)");
 
     return (
@@ -20,32 +36,32 @@ const ColorPicker = () => {
                         Color Picker
                     </DropdownToggle>
                     <DropdownMenu style={{"backgroundColor": "#212529"}} right>
-                        <DropdownItem className="btn-red">Red</DropdownItem>
-                        <DropdownItem className="btn-orange">Orange</DropdownItem>
-                        <DropdownItem className="btn-yellow">Yellow</DropdownItem>
-                        <DropdownItem className="btn-green">Green</DropdownItem>
-                        <DropdownItem className="btn-blue">Blue</DropdownItem>
-                        <DropdownItem className="btn-pink">Pink</DropdownItem>
-                        <DropdownItem className="btn-purple">Purple</DropdownItem>
-                        <DropdownItem className="btn-brown">Brown</DropdownItem>
-                        <DropdownItem className="btn-black">Black</DropdownItem>
-                        <DropdownItem className="btn-white">White</DropdownItem>
+                        <DropdownItem className="btn-red" onClick={() => dispatch({ type: STORE, payload: RED })}>Red</DropdownItem>
+                        <DropdownItem className="btn-orange" onClick={() => dispatch({ type: STORE, payload: ORANGE })}>Orange</DropdownItem>
+                        <DropdownItem className="btn-yellow" onClick={() => dispatch({ type: STORE, payload: YELLOW })}>Yellow</DropdownItem>
+                        <DropdownItem className="btn-green" onClick={() => dispatch({ type: STORE, payload: GREEN })}>Green</DropdownItem>
+                        <DropdownItem className="btn-blue" onClick={() => dispatch({ type: STORE, payload: BLUE })}>Blue</DropdownItem>
+                        <DropdownItem className="btn-pink" onClick={() => dispatch({ type: STORE, payload: PINK })}>Pink</DropdownItem>
+                        <DropdownItem className="btn-purple" onClick={() => dispatch({ type: STORE, payload: PURPLE })}>Purple</DropdownItem>
+                        <DropdownItem className="btn-brown" onClick={() => dispatch({ type: STORE, payload: BROWN })}>Brown</DropdownItem>
+                        <DropdownItem className="btn-black" onClick={() => dispatch({ type: STORE, payload: BLACK })}>Black</DropdownItem>
+                        <DropdownItem className="btn-white" onClick={() => dispatch({ type: STORE, payload: WHITE })}>White</DropdownItem>
                     </DropdownMenu>
                 </UncontrolledDropdown>
             )
             :
             (
                 <ButtonGroup className="ColorPicker" id="showColor">
-                    <Button color="red">Red</Button>
-                    <Button color="orange">Orange</Button>
-                    <Button color="yellow">Yellow</Button>
-                    <Button color="green">Green</Button>
-                    <Button color="blue">Blue</Button>
-                    <Button color="pink">Pink</Button>
-                    <Button color="purple">Purple</Button>
-                    <Button color="brown">Brown</Button>
-                    <Button color="black">Black</Button>
-                    <Button color="white">White</Button>
+                    <Button color="red" onClick={() => dispatch({ type: STORE, payload: RED })}>Red</Button>
+                    <Button color="orange" onClick={() => dispatch({ type: STORE, payload: ORANGE })}>Orange</Button>
+                    <Button color="yellow" onClick={() => dispatch({ type: STORE, payload: YELLOW })}>Yellow</Button>
+                    <Button color="green" onClick={() => dispatch({ type: STORE, payload: GREEN })}>Green</Button>
+                    <Button color="blue" onClick={() => dispatch({ type: STORE, payload: BLUE })}>Blue</Button>
+                    <Button color="pink" onClick={() => dispatch({ type: STORE, payload: PINK })}>Pink</Button>
+                    <Button color="purple" onClick={() => dispatch({ type: STORE, payload: PURPLE })}>Purple</Button>
+                    <Button color="brown" onClick={() => dispatch({ type: STORE, payload: BROWN })}>Brown</Button>
+                    <Button color="black" onClick={() => dispatch({ type: STORE, payload: BLACK })}>Black</Button>
+                    <Button color="white" onClick={() => dispatch({ type: STORE, payload: WHITE })}>White</Button>
                 </ButtonGroup>
             )
         }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { RESET } from '../store/actionTypes';
+import { useSelector, useDispatch } from 'react-redux';
 import { 
     Navbar, 
     NavItem, 
@@ -8,16 +10,20 @@ import {
 import ColorPicker from './ColorPicker';
 
 const Header = () => {
+    const currentColor = useSelector((state) => state.currentColor);
+    const dispatch = useDispatch();
+    // You need to use the RESET action creator to reset all of the nodes background colors, as well as currentColor to the standard gray. 
+
     return (
         <Navbar className="Navbar" dark>
             <NavbarBrand className="title">
                 Light-Bright
             </NavbarBrand>
             <NavItem>
-                <Button className="reset-button">Reset</Button>
+                <Button id="reset-button" onClick={() => dispatch({ type: RESET})}>Reset</Button>
             </NavItem>
-            <NavItem id="show-color">
-                <Button className="node" disabled />
+            <NavItem className="show-color">
+                <Button className="node" style={{ backgroundColor: `${currentColor}`}} disabled />
             </NavItem>
             <ColorPicker />
         </Navbar>
