57ec2e7af4a7bbb4148a38b2d095a472555924ec
Kelly <lawrencek1992@gmail.com>
2021-06-27 21:30:48 -0600

    Add payload values

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -3,15 +3,14 @@ import { STORE, UPDATE, RESET } from './actionTypes';
 export const storeColor = (newColor) => {
     return {
         type: STORE,
-        newColor: newColor,
+        payload: newColor,
     }
 };
 
 export const updateColor = (node, newColor) => {
     return {
         type: UPDATE,
-        node: node,
-        newColor: newColor,
+        payload: {node, newColor}
     }
 };
 
