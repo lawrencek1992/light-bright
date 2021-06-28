102036957e208c00db4a9b9f8f0d6fedd03444e4
Kelly <lawrencek1992@gmail.com>
2021-06-27 21:33:04 -0600

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,22 +2,16 @@ import { STORE, UPDATE, RESET } from './actionTypes';
 import { initialData } from './initialData';
 
 export default function reducer (storeData, action) {
+    let updatedStoreData = {...storeData};
     switch (action.type) {
         case STORE:
-            return {
-                ...storeData,
-                currentColor: action.newColor
-            }
-        case UPDATE: 
-            return {
-                ...storeData,
-                [action.node]: action.newColor
-            }
-        case RESET:
-            const storeArray = [...storeData]
-            let updatedStoreData = {}
-            storeArray.map(item => updatedStoreData[item] = "#343a40")
+            updatedStoreData.currentColor = action.payload;
             return updatedStoreData;
+        case UPDATE: 
+            updatedStoreData.payload.node = action.payload.newColor;
+            return updatedStoreData;
+        case RESET:
+            return initialData;
         default: 
             return storeData || initialData;
         ;
