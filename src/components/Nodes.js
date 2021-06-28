cdff361d5b020b9d632ff0849f38655413642dbd
Kelly <lawrencek1992@gmail.com>
2021-06-27 21:30:37 -0600

    Update Nodes.js

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -5,6 +5,9 @@ import {
 } from 'reactstrap'; 
 
 const Nodes = () => {
+    // You need to use the state for each node to define it's current background-color. 
+    // You need an action creator to change the color of each node when clicked to "currentColor"
+
     const nodesGrid = () => {
         let nodesArray = [];
         for (let i = 1; i <= 1300; i++) {
