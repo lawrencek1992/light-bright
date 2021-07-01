1446ac9c76a884292dfbe653ffeedd364ca76a9d
Kelly <lawrencek1992@gmail.com>
2021-06-30 21:27:33 -0600

    Store nodes in an array in the store

--- src/store/initialData.js
+++ src/store/initialData.js
@@ -1,13 +1,14 @@
 let nodesArray = [];
-for (let i = 1; i <= 1300; i++) {
+for (let i = 0; i <= 1299; i++) {
     nodesArray.push(i);
 }
-let initialNodeColors = {};
-initialNodeColors.currentColor = "#343a40";
-nodesArray.forEach(element => {
-    let nodeID = element;
+let initialNodeColors = {
+    currentColor: "#343a40",
+    nodes: [],
+};
+nodesArray.forEach(node => {
     let nodeColor = "#343a40"; // $gray-800
-    initialNodeColors[nodeID] = nodeColor;
+    initialNodeColors.nodes[node] = nodeColor;
 })
 
 export const initialData = initialNodeColors;
