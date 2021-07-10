19a526a8b53d0190ae999b26c170c3945aa60f57
Kelly Lawrnece <lawrencek1992@gmail.com>
2021-07-09 20:34:13 -0600

    Update Instructions.js

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -13,7 +13,31 @@ const Instructions = ({ showInstructions, setShowInstructions }) => {
     return (
         <Modal className="Instructions" isOpen={showInstructions} toggle={toggle} fade centered>
             <ModalHeader toggle={toggle}>Light-Bright Instructions</ModalHeader>
-            <ModalBody>To use Light-Bright select a color, then click on a node to change it to that color. You can use the Reset button to erase your work.</ModalBody>
+            <ModalBody>
+                <ol>
+                    <li>
+                       <strong>
+                       Use the color selector to select a color. 
+                       </strong> Click on any node to change its color. 
+                    </li>
+                    <br />
+                    <li>
+                        <strong>
+                            Double click
+                        </strong> on any node in order to <strong>  reset </strong> it to its original color.
+                    </li>
+                    <br />
+                    <li>
+                        Use the <strong> Reset button </strong> to erase all of your work and reset the entire board. 
+                    </li>
+                    <br />
+                    <em>
+                        <li>
+                            If you resize the screen while you are working, your image will become distorted. 
+                        </li>
+                    </em>
+                </ol>
+            </ModalBody>
             <ModalFooter>
                 <Button className="btn-gray" onClick={() => setShowInstructions(false)}>Close</Button>
             </ModalFooter>
